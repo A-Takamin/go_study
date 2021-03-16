58a23fb036de057603104a1938b004c4f29fe4a8
A-Takamin <takamin0609@gmail.com>
2021-03-16 22:50:14 +0900

    switch

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
+	sum(3, 5)
+
 	// for文
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
@@ -16,4 +18,33 @@ func main() {
 	for n < 10 {
 		fmt.Println(n)
 	}
+
+	// 無限ループとbreakとcontinue
+	n = 0
+	for {
+		n++
+		if n < 10 {
+			break
+		}
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Print(n)
+	}
+
+	// switch。fallthroughで次のcaseへまたぐ。
+	n = 10
+	switch n {
+	case 5:
+		fmt.Println("hoge")
+	case 10, 15:
+		fmt.Println("fuga")
+		fallthrough
+	case 20, 25:
+		fmt.Println("piyo")
+	}
+}
+
+func sum(x, y int) int {
+	return x + y
 }
