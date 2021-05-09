9dbb794835220da781c936a65b88a7920f860eb5
A-Takamin <takamin0609@gmail.com>
2021-05-09 20:40:39 +0900

    login func

---
+++ chat/auth.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// ラップ対象のハンドラを持つ構造体
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// 未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// 何らかのエラー
+		panic(err.Error())
+	} else {
+		// 成功
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+// ヘルパー関数。ラップしたハンドラーを返す。
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// loginHandlerはサードパーティへのログイン処理を受け持ちます。
+// パスの形式： /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+
+}
